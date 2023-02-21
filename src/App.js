130e44d0a5760b7bbf21f95f84b7d86879687d38
aAyari1993 <ahmed.ayari@csuglobal.edu>
2023-02-21 10:53:35 +0100

    feat: create navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import AppBa from './appBa';
+
+
+
+function App() {
+  return (
+    <div>
+      <AppBa/>
+    </div>
+  );
+}
+
+export default App;
+
